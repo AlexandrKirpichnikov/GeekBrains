92ba815cc3a2a10c0f5fb49a4b1ed6375e8d8780
Alex <k.sasha095@gmail.com>
2021-08-03 17:46:31 +0500

    3 Практическое Задание

---
+++ Lesson3/Lesson 3-1.py
@@ -0,0 +1,12 @@
+def my_func():
+    try:
+        a = int(input('Введите делимое: '))
+        b = int(input('Введите делитель: '))
+    except ValueError:
+        return
+    if b == 0:
+        print('На ноль делить нельзя!')
+    else:
+        s_full = a/b
+        return s_full
+print(my_func())
---
+++ Lesson3/Lesson 3-6.py
@@ -0,0 +1,5 @@
+def int_func(*args):
+    word = input('Введите слово с маленькой буквой: ')
+    print(word.title())
+    return
+int_func()
